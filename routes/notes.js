f05f24bce8e2866840f29018176d4fdf99b52c22
Ramprasadya <yadavramprasad563@gmail.com>
2024-03-16 12:25:36 +0530

    get notes

--- routes/notes.js
+++ routes/notes.js
@@ -1,8 +1,15 @@
-const express = require("express")
+const express = require("express");
+const fetchuser = require("../middleware/fetchuser");
+const Notes = require("../models/Notes");
 const router =  express.Router();
 
 router.post("/addnote",async(req,res)=>{
-    
+
+})
+
+router.get("/getnotes",fetchuser,async(req,res)=>{
+  let notes = await Notes.find({user :req.user.id})
+  res.send(notes)
 })
 
 
