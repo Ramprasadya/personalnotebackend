ffa0d5569c07b5e2596bdf9d17f5c77b412bba7a
Ramprasadya <yadavramprasad563@gmail.com>
2024-03-16 11:20:25 +0530

    bcrypt bug fix

--- routes/auth.js
+++ routes/auth.js
@@ -28,7 +28,7 @@ router.post(
   }
 //    password hashing  ........
     const salt = await bcrypt.genSalt(10)
-    const secpass = await bcrypt.hash(req.body.email ,salt)
+    const secpass = await bcrypt.hash(req.body.password ,salt)
       user = await User.create({
        name: req.body.name,
        email : req.body.email,
@@ -62,7 +62,7 @@ router.post(
     body("email", "Enter a valid email").isEmail(),
     body("password", "Password can not be blank").exists(),
   ],
-  async (req, res) => {
+  async(req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
@@ -71,29 +71,27 @@ router.post(
     const { email, password } = req.body;
     try {
       let user = await User.findOne({ email });
-
+      
       if (!user) {
         return res.status(400).json({ message: "Please login with correct credentials" });
       }
 
-      let comparePassword = await bcrypt.compare(password, user.password,(err ,result)=>{
-        return result == true
-      });
+      let comparePassword = await bcrypt.compare(password, user.password);
+  console.log(comparePassword)
+      if (!comparePassword) {
+        return res.status(400).json({ message: "Please login with correct credentials - " });
+      }else{
+        const data = {
+          user: {
+            id: user.id
+          }
+        };
+  
+        const authToken = jwt.sign(data, JWT_SECRET);
+        res.json({ authToken });
+      }
 
-      console.log(comparePassword);
-
-      // if (!comparePassword) {
-      //   return res.status(400).json({ message: "Please login with correct credentials - " });
-      // }
-
-      const data = {
-        user: {
-          id: user.id
-        }
-      };
-
-      const authToken = jwt.sign(data, JWT_SECRET);
-      res.json({ authToken });
+      
 
     } catch (err) {
       console.log(err);
