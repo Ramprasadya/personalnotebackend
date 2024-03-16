0266ce56a9039084de8214487b57c33ef88f0afc
Ramprasadya <yadavramprasad563@gmail.com>
2024-03-16 12:55:29 +0530

    adding the notes is done

--- middleware/fetchuser.js
+++ middleware/fetchuser.js
@@ -9,7 +9,7 @@ if(!token){
 res.status(401).send({error:"Enter a valid token"})
 }else{
     let data = jwt.verify(token,JWT_SECRET)
-    console.log(data)
+    // console.log(data)
     req.user = data.user;
     next();
 }
--- routes/notes.js
+++ routes/notes.js
@@ -1,16 +1,42 @@
 const express = require("express");
 const fetchuser = require("../middleware/fetchuser");
 const Notes = require("../models/Notes");
+const { body, validationResult } = require("express-validator");
+
 const router =  express.Router();
 
-router.post("/addnote",async(req,res)=>{
-
-})
-
+// Fetch notes 
 router.get("/getnotes",fetchuser,async(req,res)=>{
-  let notes = await Notes.find({user :req.user.id})
-  res.send(notes)
+    try {
+        
+        let notes = await Notes.find({user :req.user.id})
+        res.send(notes)
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({message:"Internal server Error"})
+    }
 })
+// add notes 
+router.post("/addnotes",fetchuser,[
+    body("title","Enter a valid email").isLength({min:3}),
+    body("desc","Enter at least 4 number ").isLength({ min: 5 }),
+],async(req,res)=>{
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const {title,desc,tag} = req.body
+    try {
+        let notes = await new Notes({title,desc,tag,user:req.user.id})
+        let savenote = await notes.save();
+        res.send(savenote);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({message:"Internal server Error"})
+    }
+
+})
+
 
 
 module.exports = router ;
